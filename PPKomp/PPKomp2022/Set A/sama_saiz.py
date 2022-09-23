be1cc74395112d89fdffa682e39cfbbd49018499
AkaruiYami <51882840+AkaruiYami@users.noreply.github.com>
2022-09-23 22:02:45 +0800

    Solution PPKomp2022 Set A Q1

---
+++ PPKomp/PPKomp2022/Set A/sama_saiz.py
@@ -0,0 +1,16 @@
+import math
+
+
+def main():
+    m = int(input())
+    n = int(input())
+
+    # r = m / n
+    # print(math.floor(r))
+
+    r = m // n
+    print(r)
+
+
+if __name__ == "__main__":
+    main()
